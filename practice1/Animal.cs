47e76d2efdd1a367ac38c0da91f0e63f832cecdc
Surplus <yingouyingou@gmail.com>
2019-05-31 11:30:22 +0900

    클래스연습(1)

---
+++ practice1/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice1
+{
+    class Animal
+    {
+        public string name;
+        public string sound;
+
+        public Animal()
+        {
+            name = "No name";
+            sound = "No sound";
+
+        }
+
+        public Animal(string n, string s)
+        {
+            name = n;
+            sound = s;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("[F] name : {0}, sound : {1}", name, sound);
+        }
+    }
+}
---
+++ practice1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal fox = new Animal();
+            fox.name = "여우";
+            fox.sound = "woof";
+            Console.WriteLine("[F] name : {0}, sound : {1}", fox.name, fox.sound);
+
+            fox.print();
+
+            Console.ReadLine();
+        }
+        
+        void practiceRectangle()
+        {
+            Rectangle rect1;
+            rect1.width = 100;
+            rect1.length = 30;
+
+            rect1.print();
+
+            // Console.WriteLine("[L] width{0}, length:{1}, Area{2}", rect1.width, rect1.length, rect1.Area());
+
+            Rectangle rect2 = new Rectangle(200, 50);
+
+            rect2.print();
+
+            //Console.WriteLine("[L] width{0},, length:{1}, Area{2}", rect2.width, rect2.length, rect2.Area());
+        }
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l, double w)
+            {
+                length = l;
+                width = w;
+            }
+
+            public void print()
+            {
+                Console.WriteLine("[L] width{0}, length:{1}, Area{2}", width, length, Area());
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+        }
+    }
+}
